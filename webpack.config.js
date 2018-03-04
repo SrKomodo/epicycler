f4b289a26ac7ea3b725c3af06730711a927d0844
SrKomodo <komodosr@gmail.com>
2018-03-04 12:53:44 -0300

    make several tweaks to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-module.exports = {
+module.exports = env => ({
+  mode: env.prod ? "production" : "development",
   entry: require("path").resolve(__dirname, "src", "index.ts"),
+  output: { filename: "bundle.[chunkhash].js" },
   resolve: {
     extensions: [".js", ".ts"]
   },
@@ -19,16 +23,34 @@ module.exports = {
       },
       {
         test: /.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: { plugins: env.prod ? [
+                require("autoprefixer"), require("cssnano")
+              ] : [
+                require("autoprefixer")
+              ]}
+            }
+          ]
+        })
       }
     ]
   },
+  optimization: {
+    runtimeChunk: env.prod ? true : false
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html"
-    })
+      template: "src/index.html",
+      minify: env.dev ? false : {
+        collapseWhitespace: true
+      }
+    }),
+    new ExtractTextPlugin("bundle.[contenthash].css"),
+    new CleanWebpackPlugin("dist")
   ]
-}
+});
