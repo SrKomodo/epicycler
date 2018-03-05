f3b1c51d61eaa7f7d986f2582f784250028d5d8f
SrKomodo <komodosr@gmail.com>
2018-03-05 11:49:57 -0300

    move rendering to main file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import Epicycle from "./epicycle";
-import render from "./renderer";
 import "./style.css";
 import { random } from "./utils";
 
-interface Point {
-  x: number;
-  y: number;
+const ctx = (document.getElementById("canvas") as HTMLCanvasElement).getContext("2d", { alpha: false })!;
+const path = new Path2D();
+
+let t = 0;
+let epicycle = random(100);
+const size = epicycle.size + 4;
+
+ctx.canvas.width = ctx.canvas.height = size;
+
+ctx.fillStyle = "#232323";
+ctx.fillRect(0, 0, size, size);
+
+function iterate() {
+  ctx.fillRect(0, 0, size, size);
+  ctx.beginPath();
+  epicycle.draw(t, size / 2, size / 2, ctx, path);
+  ctx.strokeStyle = "#bf4040";
+  ctx.stroke(path);
+  ctx.strokeStyle = "#808080";
+  ctx.stroke();
+
+  t += .01;
+  requestAnimationFrame(iterate);
 }
-
-const epicycle = new Epicycle(
-  1,
-  50,
-  0,
-  new Epicycle(
-    2,
-    25,
-  ),
-);
-
-render(random(100));
+requestAnimationFrame(iterate);
