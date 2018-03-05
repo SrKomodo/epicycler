a7f29001f3940a7ddec8824ad000f45cc5a1654b
SrKomodo <komodosr@gmail.com>
2018-03-05 12:58:42 -0300

    improve sourcemaps in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-module.exports = env => ({
-  mode: env.prod ? "production" : "development",
-  entry: require("path").resolve(__dirname, "src", "index.ts"),
-  output: { filename: "bundle.[chunkhash].js" },
-  resolve: {
-    extensions: [".js", ".ts"]
-  },
-  module: {
-    rules: [
-      {
-        test: /.ts$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: { presets: ["env"] }
-          },
-          "ts-loader"
-        ]
-      },
-      {
-        test: /.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+module.exports = env => {
+  const dev = env.dev ? true : false;
+  const prod = env.prod ? true : false;
+  return {
+    entry: require("path").resolve(__dirname, "src", "index.ts"),
+    output: { filename: "bundle.[chunkhash].js" },
+
+    mode: env.prod ? "production" : "development",
+    devtool: env.dev ? "inline-source-map" : undefined,
+    resolve: { extensions: [".js", ".ts"] },
+
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
           use: [
-            "css-loader",
             {
-              loader: "postcss-loader",
-              options: { plugins: env.prod ? [
-                require("autoprefixer"), require("cssnano")
-              ] : [
-                require("autoprefixer")
-              ]}
-            }
+              loader: "babel-loader",
+              options: { presets: ["env"], sourceMaps: dev }
+            },
+            "ts-loader"
           ]
-        })
-      }
+        },
+        {
+          test: /.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: { loader: "style-loader", options: { sourceMap: dev }},
+            use: [
+              { loader: "css-loader", options: { sourceMap: dev }},
+              {
+                loader: "postcss-loader",
+                options: {
+                  plugins: env.prod ? [
+                    require("autoprefixer"), require("cssnano")
+                  ] : [
+                    require("autoprefixer")
+                  ],
+                  sourceMap: dev
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    optimization: {
+      runtimeChunk: prod
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+        minify: env.dev ? false : {
+          collapseWhitespace: true
+        }
+      }),
+      new ExtractTextPlugin("bundle.[contenthash].css"),
+      new CleanWebpackPlugin("dist")
     ]
-  },
-  optimization: {
-    runtimeChunk: env.prod ? true : false
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      minify: env.dev ? false : {
-        collapseWhitespace: true
-      }
-    }),
-    new ExtractTextPlugin("bundle.[contenthash].css"),
-    new CleanWebpackPlugin("dist")
-  ]
-});
+  };
+};
