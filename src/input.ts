0658d4b26be0ab6e5a06d3064bc687dcdbfb766a
SrKomodo <komodosr@gmail.com>
2018-03-05 23:07:33 -0300

    add help menu and logging

--- src/input.ts
+++ src/input.ts
@@ -2,27 +2,45 @@ import { state } from "./index";
 import { setupCanvas } from "./renderer";
 import { random } from "./utils";
 
+const helpDialog = document.getElementById("help") as HTMLDivElement;
+const actionDialog = document.getElementById("action") as HTMLDivElement;
+
+function update(text: string) {
+  const element = document.createElement("div");
+  element.innerHTML = text;
+  element.className = "fadeout";
+  actionDialog.appendChild(element);
+  element.addEventListener("animationend", () => {
+    element.remove();
+  });
+}
+
 function startInput() {
   document.addEventListener("keypress", (e) => {
     switch (e.key) {
       case ",":
         state.speed /= 2;
+        update(`speed=${state.speed * 100}`);
         break;
       case ".":
         state.speed *= 2;
+        update(`speed=${state.speed * 100}`);
+        break;
+      case "c":
+        state.path = new Path2D();
+        update("path cleared");
         break;
       case "r":
         state.path = new Path2D();
         state.t = 0;
+        update("animation restarted");
         break;
       case "n":
         state.path = new Path2D();
         state.t = 0;
         state.epicycle = random(100, state.mode[0], state.mode[1]);
         setupCanvas();
-        break;
-      case "c":
-        state.path = new Path2D();
+        update("new epicycle generated");
         break;
       case "m":
         if (state.mode[0]) {
@@ -35,7 +53,11 @@ function startInput() {
         state.t = 0;
         state.epicycle = random(100, state.mode[0], state.mode[1]);
         setupCanvas();
+        update(`mode switched to ${state.mode[0] ? "looping " : " "}${state.mode[1] ? "circles" : "chaos"}`);
         break;
+
+      case "F1":
+        helpDialog.classList.toggle("hidden");
     }
   });
 }
